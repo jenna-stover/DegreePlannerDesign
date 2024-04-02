47f97ed712141a6284aab615a7b8d3a2494e5408
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-02 14:30:13 -0400

    added json.simple

--- design/src/main/java/module-info.java
+++ design/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module degreeplanner.design {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens degreeplanner.design to javafx.fxml;
     exports degreeplanner.design;
