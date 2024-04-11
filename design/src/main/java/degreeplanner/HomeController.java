e83707ffd360ce99cf4cf1e95aa274635bc45a47
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-11 13:36:19 -0400

    added homecontroller skeleton

--- design/src/main/java/degreeplanner/HomeController.java
+++ design/src/main/java/degreeplanner/HomeController.java
@@ -1,19 +1,41 @@
 package degreeplanner;
 
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-
-import javafx.event.ActionEvent;
+import com.gluonhq.charm.glisten.control.Avatar;
+import com.gluonhq.charm.glisten.control.DropdownButton;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-public class HomeController implements Initializable {
-    @FXML
-    private void onLoginClicked(ActionEvent event) throws IOException {
-        App.setRoot("/login");
-    }
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.text.Text;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-    }
+public class HomeController {
+
+    @FXML
+    private TextArea adv_note1;
+
+    @FXML
+    private TextArea adv_note2;
+
+    @FXML
+    private TextArea adv_note3;
+
+    @FXML
+    private Avatar avatar_profile;
+
+    @FXML
+    private Text degreeplanner_home;
+
+    @FXML
+    private ScrollBar notes_scrollbar;
+
+    @FXML
+    private ProgressBar progress_bar;
+
+    @FXML
+    private DropdownButton semester_dropdown;
+
+    @FXML
+    private Label user_name_profile;
+
 }
