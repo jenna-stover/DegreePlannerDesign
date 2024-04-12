2e1d2eeed6e5cc635d284b4b322b99a7f6195d02
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-11 20:44:38 -0400

    updated HomeController class

--- design/src/main/java/degreeplanner/HomeController.java
+++ design/src/main/java/degreeplanner/HomeController.java
@@ -1,26 +1,25 @@
 package degreeplanner;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollBar;
-import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import degreeplanner.design_code.HomeFacade;
 
-public class HomeController {
-
-    @FXML
-    private TextArea adv_note1;
-
-    @FXML
-    private TextArea adv_note2;
+public class HomeController implements Initializable{
 
     @FXML
-    private TextArea adv_note3;
+    private VBox adv_notes_vbox;
 
     @FXML
     private Text degreeplanner_home;
@@ -31,13 +30,37 @@ public class HomeController {
     @FXML
     private ProgressBar progress_bar;
 
+    @FXML
+    private Text search_course;
+
+    @FXML
+    private ImageView search_img;
+
+    @FXML
+    private VBox semester_courses_vbox;
+
+    @FXML
+    private ChoiceBox<?> semester_dropdown;
 
     @FXML
     private Label user_name_profile;
 
     @FXML
+    void avatarProfileClicked(MouseEvent event) throws IOException {
+        App.setRoot("/profile");
+    }
+
+    @FXML
     void goHome(MouseEvent event) throws IOException {
         App.setRoot("/home");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        if(homeFacade.getUser() != null){
+            user_name_profile.setText(homeFacade.getUser());
+        }
+        
     }
 
 }
