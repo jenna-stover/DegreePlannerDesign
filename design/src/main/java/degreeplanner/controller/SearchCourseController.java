8cfeacedcc00cc6570988190c16d75a0d8525c29
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-15 13:24:58 -0400

    updated search_courses fxml

--- design/src/main/java/degreeplanner/controller/SearchCourseController.java
+++ design/src/main/java/degreeplanner/controller/SearchCourseController.java
@@ -1,24 +1,57 @@
 package degreeplanner.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import degreeplanner.App;
+import degreeplanner.design_code.HomeFacade;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
-public class SearchCourseController {
+public class SearchCourseController implements Initializable{
 
     @FXML
     private Text degreeplanner_search;
 
     @FXML
+    private VBox searchCourseVBox;
+
+    @FXML
+    private Button search_button;
+
+    @FXML
     private Pane search_pane;
+
+    @FXML
+    private Label user_fullname;
+
+    @FXML
+    private ImageView user_profile;
 
     @FXML
     void goHome(MouseEvent event) throws IOException {
       App.setRoot("/fxml/home");
+    }
+
+    @FXML
+    void goToProfile(MouseEvent event) throws IOException {
+      App.setRoot("/fxml/profile");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        HomeFacade homeFacade = HomeFacade.getInstance();
+        if(homeFacade.getLoggedInUser() != null){
+            user_fullname.setText(homeFacade.getLoggedInUser().getUserFullName());
+        }
     }
 
 }
