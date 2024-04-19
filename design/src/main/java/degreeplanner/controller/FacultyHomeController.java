c7296a6338dfb7c7b7dad1ea89399f2653e75bd9
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-19 17:40:51 -0400

    storing changes

---
+++ design/src/main/java/degreeplanner/controller/FacultyHomeController.java
@@ -0,0 +1,47 @@
+package degreeplanner.controller;
+
+import java.io.IOException;
+
+import degreeplanner.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
+
+public class FacultyHomeController {
+
+    @FXML
+    private TableView<?> adviseeTable;
+
+    @FXML
+    private Pane facultyNotePane;
+
+    @FXML
+    private Label facultySearchCourse;
+
+    @FXML
+    private Label facultySearchUser;
+
+    @FXML
+    void goToLogin(MouseEvent event) throws IOException {
+        App.setRoot("/fxml/login");
+    }
+
+    @FXML
+    void goToProfile(MouseEvent event) throws IOException {
+        App.setRoot("/fxml/facultyProfile");
+    }
+
+    @FXML
+    void goToSearchCourse(MouseEvent event) throws IOException {
+        App.setRoot("/fxml/search_course");
+    }
+
+    @FXML
+    void goToSearchUser(MouseEvent event) throws IOException {
+        App.setRoot("/fxml/searchUser");
+    }
+
+}
