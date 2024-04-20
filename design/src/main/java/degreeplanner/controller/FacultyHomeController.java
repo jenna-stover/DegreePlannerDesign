d4da194c4d48b824fea3f631c3996266eb737d59
jenna-stover.github.io <jenna.stover810@gmail.com>
2024-04-20 15:00:09 -0400

    updated FacultyHomeController

--- design/src/main/java/degreeplanner/controller/FacultyHomeController.java
+++ design/src/main/java/degreeplanner/controller/FacultyHomeController.java
@@ -1,16 +1,20 @@
 package degreeplanner.controller;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import degreeplanner.App;
+import degreeplanner.design_code.HomeFacade;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 
-public class FacultyHomeController {
+public class FacultyHomeController implements Initializable{
 
     @FXML
     private TableView<?> adviseeTable;
@@ -24,36 +28,40 @@ public class FacultyHomeController {
     @FXML
     private Label facultySearchUser;
 
+    @FXML
+    private Button logout_button;
 
+    @FXML
+    private Label profileFullName;
 
     @FXML
     void goToProfile(MouseEvent event) throws IOException {
         App.setRoot("/fxml/facultyProfile");
-
     }
 
     @FXML
     void goToSearchCourse(MouseEvent event) throws IOException {
-
         App.setRoot("/fxml/search_course");
-
     }
 
     @FXML
     void goToSearchUser(MouseEvent event) throws IOException {
-
         App.setRoot("/fxml/searchUser");
-
     }
 
     @FXML
-    void userLogout(MouseEvent event) {
-
+    void userLogout(MouseEvent event) throws IOException {
+        App.setRoot("/fxml/facultyLogin");
     }
 
-    @FXML
-    void goToLogin(MouseEvent event) throws IOException {
-        App.setRoot("/fxml/login");
+    @Override
+    public void initialize(URL location, ResourceBundle resources) 
+    {
+        HomeFacade homeFacade = HomeFacade.getInstance();
+        if(homeFacade.getLoggedInUser() != null)
+        {
+            profileFullName.setText(homeFacade.getLoggedInUser().getUserFullName());
+        }
     }
 
 }
